5a4fcba4b4bc706d36f74c81ac10363378299174
Jon <thejunkjon@gmail.com>
2019-04-10 10:01:44 -0700

    Adding linking to v8

--- app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
+++ app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.github.thejunkjon.androidintrospection
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.thejunkjon.lib.HookManager
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        HookManager().applyHooks()
     }
 }
---
+++ lib/src/main/cpp/v8_wrapper.h
@@ -0,0 +1,12 @@
+#ifndef V8_WRAPPER_H
+#define V8_WRAPPER_H
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+#include <libplatform/libplatform.h>
+#include <v8.h>
+
+#pragma GCC diagnostic pop
+
+#endif // V8_WRAPPER_H
