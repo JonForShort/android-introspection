5295c56cd0310e752c8d69ae9e719bc9af60aabf
Jon <thejunkjon@gmail.com>
2019-09-13 23:40:45 -0700

    initial check-in of listing installed apps on MainActivity

--- app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
+++ app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.github.thejunkjon.androidintrospection
 
+import android.content.Intent
+import android.content.pm.ResolveInfo
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.github.thejunkjon.lib.HookManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        init {
-            System.loadLibrary("native-lib")
-        }
-    }
+    private lateinit var mInstalledApps: List<ResolveInfo>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        HookManager().applyHooks()
+
+        updateInstalledApps()
+
+        installed_apps.adapter = AppsAdapter()
+    }
+
+    private fun updateInstalledApps() {
+        val mainIntent = Intent(Intent.ACTION_MAIN, null)
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        mInstalledApps = packageManager.queryIntentActivities(mainIntent, 0)
+    }
+
+    private inner class AppsAdapter : BaseAdapter() {
+
+        override fun getCount(): Int {
+            return mInstalledApps.size
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val i: ImageView
+            if (convertView == null) {
+                i = ImageView(this@MainActivity)
+            } else {
+                i = convertView as ImageView
+            }
+            val info = mInstalledApps[position]
+            i.setImageDrawable(info.activityInfo.loadIcon(packageManager))
+            return i
+        }
+
+        override fun getItem(position: Int): Any {
+            return mInstalledApps.get(position)
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
     }
 }
