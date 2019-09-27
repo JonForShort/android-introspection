d29d8b276b3c4b3268e79d7256a7b2a9d1aac626
Jon <thejunkjon@gmail.com>
2019-09-26 23:45:14 -0700

    adding copyrights and fixing minor lint warnings

--- app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
+++ app/src/main/java/com/github/thejunkjon/androidintrospection/MainActivity.kt
@@ -1,3 +1,26 @@
+//
+// MIT License
+//
+// Copyright 2019
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
 package com.github.thejunkjon.androidintrospection
 
 import android.content.Intent
@@ -25,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         updateInstalledApps()
 
         installed_apps.adapter = AppsAdapter()
-        installed_apps.setOnItemClickListener { adapterView, view, position, l ->
+        installed_apps.setOnItemClickListener { _, _, position, _ ->
             val clickedItem = installedApps[position]
             d("item clicked : position [$position] item [$clickedItem]")
             ApkProcessor(clickedItem.activityInfo.applicationInfo.sourceDir).makeDebuggable()
