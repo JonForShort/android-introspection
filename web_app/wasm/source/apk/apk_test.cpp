c25534bf25bbd128ae9b9b91c65d37168bee9048
Jon <thejunkjon@gmail.com>
2019-11-09 01:52:02 -0800

    Adding assertion for if the apk is actually debuggable.

--- web_app/wasm/source/apk/apk_test.cpp
+++ web_app/wasm/source/apk/apk_test.cpp
@@ -23,10 +23,13 @@
 //
 #include <gtest/gtest.h>
 
+#include <filesystem>
 #include <memory>
 #include <string>
 
 #include "apk/apk.h"
+
+namespace fs = std::filesystem;
 
 namespace {
 
@@ -54,9 +57,35 @@ auto setEnvironmentIfReady() -> bool {
   return true;
 }
 
+auto checkIfDebuggable(char const *apkPath) -> bool {
+  auto apkAnalyzer = fs::path(gTestEnvironment->androidHomeDir) / "tools" / "bin" / "apkanalyzer";
+  auto command = apkAnalyzer.string() + " manifest debuggable " + apkPath;
+  auto handle = popen(command.c_str(), "r");
+  if (handle == nullptr) {
+    return false;
+  }
+
+  std::string bufferString;
+  static constexpr auto bufferSize = 80;
+  char bufferBytes[bufferSize] = {0};
+  while (fgets(bufferBytes, bufferSize, handle)) {
+    bufferString += bufferBytes;
+  }
+
+  auto exitStatus = WEXITSTATUS(pclose(handle));
+  return exitStatus == 0 && bufferString == "true";
+}
+
 } // namespace
 
-TEST(MakeDebuggable, ReleaseApkIsDebuggable) {}
+TEST(MakeDebuggable, ReleaseApkIsDebuggable) {
+  auto testApk = fs::path(gTestEnvironment->testsDir) / "apks" / "test_release.apk";
+  auto isSuccessful = ai::apk::makeApkDebuggable(testApk.c_str());
+  EXPECT_TRUE(isSuccessful);
+
+  auto isDebuggable = checkIfDebuggable(testApk.c_str());
+  EXPECT_TRUE(isDebuggable);
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
