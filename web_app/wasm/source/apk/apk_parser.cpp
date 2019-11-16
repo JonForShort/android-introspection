82786ae3510cf01213fb4b92ad8dc06f9276b22d
Jon <thejunkjon@gmail.com>
2019-11-15 21:54:53 -0800

    Implementing parts of the apk parser.  This will currently handle only reading file contents and read file names.

--- web_app/wasm/source/apk/apk_parser.cpp
+++ web_app/wasm/source/apk/apk_parser.cpp
@@ -21,4 +21,100 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+#include <cstdint>
+#include <zip.h>
+
+#include "utils/log.h"
+
 #include "apk_parser.h"
+
+using namespace ai;
+
+namespace {
+
+struct UnzOpenFile {
+
+  explicit UnzOpenFile(const char *szFileName) : zip_(unzOpen(szFileName)) {}
+
+  ~UnzOpenFile() { unzClose(zip_); }
+
+  auto get() const -> unzFile { return zip_; }
+
+private:
+  unzFile zip_;
+};
+
+struct UnzOpenCurrentFile {
+
+  explicit UnzOpenCurrentFile(unzFile const zipFile) : zipFile_(zipFile), result_(unzOpenCurrentFile(zipFile)) {}
+
+  ~UnzOpenCurrentFile() {
+    if (result_ == MZ_OK) {
+      unzCloseCurrentFile(zipFile_);
+    }
+  }
+
+  auto result() const -> int { return result_; }
+
+private:
+  unzFile const zipFile_;
+  int const result_;
+};
+
+} // namespace
+
+auto ApkParser::getFileNames() const -> std::vector<std::string> {
+  std::vector<std::string> fileNames;
+  auto szFileName = pathToApk_.c_str();
+  auto const zipFile = UnzOpenFile(szFileName);
+  if (zipFile.get() == nullptr) {
+    LOGW("unable to open zip file [%s]", szFileName);
+    return fileNames;
+  }
+  if (auto result = unzGoToFirstFile(zipFile.get()); result != UNZ_OK) {
+    LOGW("unzGoToFirstFile, result=[%d] szFileName=[%s]", result, szFileName);
+    return fileNames;
+  }
+  do {
+    unz_file_info zipFileInfo;
+    char szFilename[BUFSIZ] = {0};
+    if (unzGetCurrentFileInfo(zipFile.get(), &zipFileInfo, szFilename, sizeof(szFilename), nullptr, 0, nullptr, 0) == UNZ_OK) {
+      auto const fileName = std::string(szFilename);
+      fileNames.emplace_back(fileName);
+    }
+  } while (unzGoToNextFile(zipFile.get()) == UNZ_OK);
+  return fileNames;
+}
+
+auto ApkParser::getFileContents(char const *szZipFileName) const -> std::vector<uint8_t> {
+  auto contents = std::vector<uint8_t>();
+  auto const szFileName = pathToApk_.c_str();
+  auto const zipFile = UnzOpenFile(szFileName);
+  if (zipFile.get() == nullptr) {
+    LOGW("zipFile.get(), szFileName=[%s]", szFileName);
+    return contents;
+  }
+  if (auto result = unzLocateFile(zipFile.get(), szZipFileName, nullptr); result != MZ_OK) {
+    LOGW("unzLocateFile, result=[%d] szFileName=[%s] szZipFileName=[%s]", result, szFileName, szZipFileName);
+    return contents;
+  }
+  auto const openedZipFile = UnzOpenCurrentFile(zipFile.get());
+  if (auto result = openedZipFile.result(); result != MZ_OK) {
+    LOGW("unzOpenCurrentFile, result=[%d] szFileName=[%s] szZipFileName=[%s]", result, szFileName, szZipFileName);
+    return contents;
+  }
+  unz_file_info zipFileInfo;
+  if (auto result = unzGetCurrentFileInfo(zipFile.get(), &zipFileInfo, nullptr, 0, nullptr, 0, nullptr, 0); result != UNZ_OK) {
+    LOGW("unzGetCurrentFileInfo, result=[%d] szFileName=[%s] szZipFileName=[%s]", result, szFileName, szZipFileName);
+    return contents;
+  }
+  auto const compressedContentsSizeInBytes = zipFileInfo.uncompressed_size;
+  contents.resize(compressedContentsSizeInBytes);
+  unzReadCurrentFile(zipFile.get(), &contents[0], static_cast<uint32_t>(contents.size()));
+  return contents;
+}
+
+auto ApkParser::setFileContents(char const *fileName, std::vector<uint8_t> const &content) const -> void {
+  (void)fileName;
+  (void)content;
+}
