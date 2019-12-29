3dc273ef00e373fb9a3e5aa8b9f2d52d04bbe006
Jon <6559571+thejunkjon@users.noreply.github.com>
2019-12-28 23:38:53 -0800

    Update env.sh
    Fixing formatting

--- scripts/env.sh
+++ scripts/env.sh
@@ -12,64 +12,59 @@ ai_root_dir()
 ai_setup_environment()
 {
     if [[ -z "${AI_ANDROID_HOME}" ]]; then
-	if [[ -z "${ANDROID_HOME}" ]]; then
-	    export AI_ANDROID_HOME=${ROOT_DIR}/external/android/sdk
-	else
-	    export AI_ANDROID_HOME=${ANDROID_HOME}
-	fi
+	    if [[ -z "${ANDROID_HOME}" ]]; then
+	      export AI_ANDROID_HOME=${ROOT_DIR}/external/android/sdk
+	    else
+	      export AI_ANDROID_HOME=${ANDROID_HOME}
+	    fi
     fi
 
     if [[ -z "${AI_TESTS_DIR}" ]]; then
-	export AI_TESTS_DIR=${ROOT_DIR}/web_app/wasm/test
+        export AI_TESTS_DIR=${ROOT_DIR}/web_app/wasm/test
     fi
 }
 
-ai_build() {
+ai_build()
+{
     ai_build_android && ai_build_wasm && ai_build_host
 }
 
 ai_build_android()
-{
-    (
-	${ROOT_DIR}/android/gradlew clean build
-    )
-}
+{(
+    ${ROOT_DIR}/android/gradlew clean build
+)}
 
 ai_build_web_app()
-{
-    (
-	source ${ROOT_DIR}/external/wasm/emsdk/emsdk_env.sh
+{(
+    source ${ROOT_DIR}/external/wasm/emsdk/emsdk_env.sh
 
-	BUILD_DIR=${ROOT_DIR}/web_app/build/wasm
+    BUILD_DIR=${ROOT_DIR}/web_app/build/wasm
 
-	mkdir -p ${BUILD_DIR}
+    mkdir -p ${BUILD_DIR}
 
-	pushd ${BUILD_DIR}
+    pushd ${BUILD_DIR}
 
-	emconfigure cmake -DWASM=True --build ${ROOT_DIR}/web_app/wasm
+    emconfigure cmake -DWASM=True --build ${ROOT_DIR}/web_app/wasm
 
-	emmake make "$@"
+    emmake make "$@"
 
-	popd
-    )
-}
+    popd
+)}
 
 ai_build_web_app_host()
-{
-    (
-	BUILD_DIR=${ROOT_DIR}/web_app/build/host
+{(
+    BUILD_DIR=${ROOT_DIR}/web_app/build/host
 
-	mkdir -p ${BUILD_DIR}
+    mkdir -p ${BUILD_DIR}
 
-	pushd ${BUILD_DIR}
+    pushd ${BUILD_DIR}
 
-	cmake -DWASM=False -DCMAKE_BUILD_TYPE=Debug --build ${ROOT_DIR}/web_app/wasm
+    cmake -DWASM=False -DCMAKE_BUILD_TYPE=Debug --build ${ROOT_DIR}/web_app/wasm
 
-	make "$@" && make test
+    make "$@" && make test
 
-	popd
-    )
-}
+    popd
+)}
 
 ai_setup_environment
 
