322205a7539d8739fb8b6a8a7057db96d73f7312
Jon <thejunkjon@gmail.com>
2020-05-24 04:13:12 -0700

    Adding chrome to docker file for tests.  Updating docker compose file to expose ports as-is.

--- web_app/app/src/app/app.component.ts
+++ web_app/app/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'app';
+
+  changeMessage = '';
+
+  onInputChanged() {
+    this.changeMessage = 'Input has changed';
+  }
 }
