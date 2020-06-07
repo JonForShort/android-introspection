f1c753dfd0dcbed98c06ac91b27a0ea77cb35af5
Jon <thejunkjon@gmail.com>
2020-06-07 02:10:31 -0700

    Adding material tabs to index page which will store the file paths for the loaded APK.

---
+++ web_app/app/src/app/app-material/app-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatTabsModule
+  ],
+  exports: [
+    MatTabsModule
+  ]
+})
+export class AppMaterialModule { }
--- web_app/app/src/app/app.module.ts
+++ web_app/app/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { AppComponent } from './app.component';
+import { AppMaterialModule } from './app-material/app-material.module'
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterialModule,
+    BrowserAnimationsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
