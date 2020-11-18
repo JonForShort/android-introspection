cde9f55ca7da947de2c20a311e81e09ddd417f0e
thejunkjon <thejunkjon@gmail.com>
2020-11-18 02:53:11 -0800

    Removing android-specific utils from wasm.

--- web_app/wasm/source/utils/include/utils/log.h
+++ web_app/wasm/source/utils/include/utils/log.h
@@ -24,50 +24,6 @@
 #ifndef ANDROID_INTROSPECTION_UTILS_LOG_H_
 #define ANDROID_INTROSPECTION_UTILS_LOG_H_
 
-#define TAG "AndroidIntrospectionNative"
-
-#ifdef ANDROID
-
-#include <android/log.h>
-
-#if LOG_LEVEL < 1
-#define LOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, TAG, __VA_ARGS__)
-#else
-#define LOGV(...)
-#endif
-
-#if LOG_LEVEL < 2
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
-#else
-#define LOGD(...)
-#endif
-
-#if LOG_LEVEL < 3
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#else
-#define LOGI(...)
-#endif
-
-#if LOG_LEVEL < 4
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-#else
-#define LOGW(...)
-#endif
-
-#if LOG_LEVEL < 5
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#else
-#define LOGE(...)
-#endif
-
-#if LOG_LEVEL < 6
-#define LOGF(...) __android_log_print(ANDROID_LOG_FATAL, TAG, __VA_ARGS__)
-#else
-#define LOGF(...)
-#endif
-
-#else
-
 #if LOG_LEVEL == 0
 #define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
 #elif LOG_LEVEL == 1
@@ -121,7 +77,5 @@
 #else
 #define LOGF(...)
 #endif
-
-#endif /* ANDROID */
 
 #endif /* ANDROID_INTROSPECTION_UTILS_LOG_H_ */
