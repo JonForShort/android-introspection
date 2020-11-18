11da141e81c1c978b85e7df21143e712fbb7fa1f
thejunkjon <thejunkjon@gmail.com>
2020-11-18 02:45:40 -0800

    Removing unnecessary code from utils.

--- android/vpn/src/main/cpp/utils/include/utils/log.h
+++ android/vpn/src/main/cpp/utils/include/utils/log.h
@@ -1,7 +1,7 @@
 //
 // MIT License
 //
-// Copyright 2019-2020
+// Copyright 2020
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,12 +21,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-#ifndef ANDROID_INTROSPECTION_UTILS_LOG_H_
-#define ANDROID_INTROSPECTION_UTILS_LOG_H_
+#ifndef ANDROID_INTROSPECTION_VPN_UTILS_LOG_H_
+#define ANDROID_INTROSPECTION_VPN_UTILS_LOG_H_
 
 #define TAG "AndroidIntrospectionNative"
-
-#ifdef ANDROID
 
 #include <android/log.h>
 
@@ -66,62 +64,4 @@
 #define LOGF(...)
 #endif
 
-#else
-
-#if LOG_LEVEL == 0
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
-#elif LOG_LEVEL == 1
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_DEBUG
-#elif LOG_LEVEL == 2
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_INFO
-#elif LOG_LEVEL == 3
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_WARN
-#elif LOG_LEVEL == 4
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_ERROR
-#elif LOG_LEVEL == 5
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_CRITICAL
-#else
-#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_OFF
-#endif
-
-#include "spdlog/spdlog.h"
-
-#if LOG_LEVEL < 1
-#define LOGV(...) spdlog::trace(__VA_ARGS__)
-#else
-#define LOGV(...)
-#endif
-
-#if LOG_LEVEL < 2
-#define LOGD(...) spdlog::debug(__VA_ARGS__)
-#else
-#define LOGD(...)
-#endif
-
-#if LOG_LEVEL < 3
-#define LOGI(...) spdlog::info(__VA_ARGS__)
-#else
-#define LOGI(...)
-#endif
-
-#if LOG_LEVEL < 4
-#define LOGW(...) spdlog::warn(__VA_ARGS__)
-#else
-#define LOGW(...)
-#endif
-
-#if LOG_LEVEL < 5
-#define LOGE(...) spdlog::error(__VA_ARGS__)
-#else
-#define LOGE(...)
-#endif
-
-#if LOG_LEVEL < 6
-#define LOGF(...) spdlog::critical(__VA_ARGS__)
-#else
-#define LOGF(...)
-#endif
-
-#endif /* ANDROID */
-
-#endif /* ANDROID_INTROSPECTION_UTILS_LOG_H_ */
+#endif /* ANDROID_INTROSPECTION_VPN_UTILS_LOG_H_ */
