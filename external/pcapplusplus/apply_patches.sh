c3165fc4470a3bb323f050efa804213765056bb5
thejunkjon <thejunkjon@gmail.com>
2020-11-23 22:28:28 -0800

    Using the correct android compiler for pcapplusplus.

--- external/pcapplusplus/apply_patches.sh
+++ external/pcapplusplus/apply_patches.sh
@@ -6,6 +6,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 patch -N -r - ${SCRIPT_DIR}/source/mk/platform.mk.linux ${SCRIPT_DIR}/patches/mk/platform.mk.linux.patch 
 
-patch -N -r - ${SCRIPT_DIR}/source/mk/install.sh.template ${SCRIPT_DIR}/patches/mk/install.sh.template.patch 
+patch -N -r - ${SCRIPT_DIR}/source/mk/install.sh.template ${SCRIPT_DIR}/patches/mk/install.sh.template.patch
+
+patch -N -r - ${SCRIPT_DIR}/source/Packet++/Makefile ${SCRIPT_DIR}/patches/Packet++/Makefile.patch
+
+patch -N -r - ${SCRIPT_DIR}/source/Common++/Makefile ${SCRIPT_DIR}/patches/Common++/Makefile.patch
+
+patch -N -r - ${SCRIPT_DIR}/source/Common++/header/IpUtils.h ${SCRIPT_DIR}/patches/Common++/header/IpUtils.h.patch
+
+patch -N -r - ${SCRIPT_DIR}/source/mk/PcapPlusPlus.mk.linux ${SCRIPT_DIR}/patches/mk/PcapPlusPlus.mk.linux.patch
 
 set +x
