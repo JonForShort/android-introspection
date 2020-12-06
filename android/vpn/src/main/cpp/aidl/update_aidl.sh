2e7836ae98a1092e2c13964043a177ae85abb8f8
thejunkjon <thejunkjon@gmail.com>
2020-12-05 18:23:24 -0800

    Adding script to help generate aidl.

---
+++ android/vpn/src/main/cpp/aidl/update_aidl.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+aidl --lang=ndk --out=$SCRIPT_DIR --header_out=$SCRIPT_DIR/include $SCRIPT_DIR/../../aidl/com/github/jonforshort/vpn/IVpnService.aidl
--- android/vpn/src/main/cpp/utils/Android.cmake
+++ android/vpn/src/main/cpp/utils/Android.cmake
@@ -1,13 +1,9 @@
 cmake_minimum_required(VERSION 3.10.2)
 
-if (ANDROID)
+include(AndroidNdkModules)
+android_ndk_import_module_cpufeatures()
 
-    include(AndroidNdkModules)
-    android_ndk_import_module_cpufeatures()
+find_library(log-lib log)
 
-    find_library(log-lib log)
-
-    target_link_libraries(utils cpufeatures)
-    target_link_libraries(utils ${log-lib})
-
-endif ()
+target_link_libraries(utils cpufeatures)
+target_link_libraries(utils ${log-lib})
