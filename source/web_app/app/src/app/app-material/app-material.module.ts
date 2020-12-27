1f27127e1341ee6c88841fa78f513d6e25cee251
thejunkjon <thejunkjon@gmail.com>
2020-12-26 19:53:44 -0800

    Adding two options to export; one for allowing to make the APK debuggable and another for injecting frida directly into APK.

--- source/web_app/app/src/app/app-material/app-material.module.ts
+++ source/web_app/app/src/app/app-material/app-material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -10,12 +11,14 @@ import { CommonModule } from '@angular/common';
   imports: [
     CommonModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatDialogModule,
     MatTabsModule,
     MatTableModule
   ],
   exports: [
     MatButtonModule,
+    MatCheckboxModule,
     MatDialogModule,
     MatTabsModule,
     MatTableModule,
--- source/web_app/app/src/app/export-apk-dialog/export-apk-dialog.component.ts
+++ source/web_app/app/src/app/export-apk-dialog/export-apk-dialog.component.ts
@@ -4,9 +4,13 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-export-apk-dialog',
   templateUrl: './export-apk-dialog.component.html',
-  styleUrls: ['./export-apk-dialog.component.css']
+  styleUrls: ['./export-apk-dialog.component.scss']
 })
 export class ExportApkDialogComponent {
+
+  isMakeDebuggableChecked = true
+
+  isInjectFridaChecked = false
 
   constructor(private dialogRef: MatDialogRef<ExportApkDialogComponent>) {
     this.dialogRef.afterClosed().subscribe(result => {
